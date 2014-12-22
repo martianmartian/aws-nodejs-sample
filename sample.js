421d3f36b11da7013eddefb8eb0e306fdfb24cbc
iamwave007 <iamwave007@hotmail.com>
2014-12-22 03:09:15 -0500

    re-arranged pipe stream into express, before getting rid of through

--- sample.js
+++ sample.js
@@ -2,56 +2,63 @@ var express=require("express");
 var app= express();
 var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
+var fs = require('fs');
+var through = require('through');
 
 
-//=======FOR FRONT=========
-
-//app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.set('port', process.env.PORT || 3000);
-app.get('/', function(req, res){
-    res.render('index.html');
-});
-app.listen(app.get('port'));
-
+//=======FOR FRONT=========
 //=======FOR FORBACK=========
 
 // //Create a bucket and upload something into it
 // //var bucketName = 'node-sdk-sample-' + uuid.v4();
 
-var fs = require('fs');
-var through = require('through');
-var numBytes = 0;
-var address_keyName = 'helloworld.jpg';
+//app.set('views', __dirname + '/views');
 
-var stream = fs.createReadStream(address_keyName);
-stream.pipe(through(function(data) {
-  this.queue(data);
-  numBytes += data.length;
-  console.log(numBytes, 'bytes written');
-}));
+app.get('/', function(req, res){
+    res.render('index.html');
 
+    var numBytes = 0;
+    var address_keyName = 'helloworld.jpg';
+    var stream = fs.createReadStream(address_keyName);
 
-
-
-
+    stream.pipe(through(function(data) {
+      this.queue(data);
+      numBytes += data.length;
+      console.log(numBytes, 'bytes written');
+    }));
 
 
-
-var bucketName = "langtimeusers"
-AWS.config.loadFromPath = ('./config.json');
-var params={Key: address_keyName, 
-            Body: stream,
-            Bucket:bucketName
-          };
-
-var s3 = new AWS.S3();
-s3.putObject(params,function(err,data){
-  if(err)
-    console.log(err)
-  else
-    console.log("Successfully uploaded data to " + bucketName + "/" + address_keyName);
 });
+app.listen(app.get('port'));
+
+
+
+
+
+
+
+
+
+
+
+//=======FOR S3=========
+
+// var bucketName = "langtimeusers"
+// AWS.config.loadFromPath = ('./config.json');
+// var params={Key: address_keyName, 
+//             Body: stream,
+//             Bucket:bucketName
+//           };
+
+// var s3 = new AWS.S3();
+// s3.putObject(params,function(err,data){
+//   if(err)
+//     console.log(err)
+//   else
+//     console.log("Successfully uploaded data to " + bucketName + "/" + address_keyName);
+// });
 
 
 
