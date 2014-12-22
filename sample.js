54a41d6b0755c14c0957cb6fe03e67f219ad4d68
iamwave007 <iamwave007@hotmail.com>
2014-12-22 05:09:32 -0500

    front end, convert image to 64 base

--- sample.js
+++ sample.js
@@ -17,18 +17,48 @@ app.set('port', process.env.PORT || 3000);
 //app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res){
-    res.render('index.html');
+    
 
     var numBytes = 0;
     var address_keyName = 'helloworld.jpg';
     var stream = fs.createReadStream(address_keyName);
 
+
+    stream.on('open',function(data){
+      stream.pipe(res);
+      res.render('index.html');
+      //res.sendStatus(data)
+    })
+
+    stream.on('error',function(err){
+      res.end(err);
+    })
+
+
+
+
     stream.pipe(through(function(data) {
       this.queue(data);
       numBytes += data.length;
       console.log(numBytes, 'bytes written');
-    }));
+    })
+    );
 
+
+    // var bucketName = "langtimeusers"
+    // AWS.config.loadFromPath = ('./config.json');
+    // var params={Key: address_keyName, 
+    //             Body: stream,
+    //             Bucket:bucketName
+    //           };
+
+    // var s3 = new AWS.S3();
+    // s3.putObject(params,function(err,data){
+    //   if(err)
+    //     console.log(err)
+    //   else
+    //     console.log("Successfully uploaded data to " + bucketName + "/" + address_keyName);
+    // });
 
 });
 app.listen(app.get('port'));
